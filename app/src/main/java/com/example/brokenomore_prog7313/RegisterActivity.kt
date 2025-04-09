83998a43ea6f1eff5e0105e8bd309ee71615c0c7
Gerhard <glemmer94@gmail.com>
2025-04-09 17:30:19 +0200

    Fixed error and implemented views

--- app/src/main/java/com/example/brokenomore_prog7313/RegisterActivity.kt
+++ app/src/main/java/com/example/brokenomore_prog7313/RegisterActivity.kt
@@ -10,6 +10,12 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.registrationActivity)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
     }
 }
